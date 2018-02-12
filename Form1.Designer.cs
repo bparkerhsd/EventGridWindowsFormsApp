9b1a34db16685be3a39a36d82fe4b57341840e07
bparkerhsd <bruce.parker@outlook.com>
2018-02-12 17:01:52 +0000

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace EventGridWindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sendEventButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EventTopicTextBox = new System.Windows.Forms.TextBox();
+            this.EventTopicLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sendEventButton
+            // 
+            this.sendEventButton.Location = new System.Drawing.Point(32, 117);
+            this.sendEventButton.Name = "sendEventButton";
+            this.sendEventButton.Size = new System.Drawing.Size(75, 23);
+            this.sendEventButton.TabIndex = 0;
+            this.sendEventButton.Text = "Send Event";
+            this.sendEventButton.UseVisualStyleBackColor = true;
+            this.sendEventButton.Click += new System.EventHandler(this.sendEvent_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Event Grid Tester";
+            // 
+            // EventTopicTextBox
+            // 
+            this.EventTopicTextBox.Location = new System.Drawing.Point(35, 79);
+            this.EventTopicTextBox.Name = "EventTopicTextBox";
+            this.EventTopicTextBox.Size = new System.Drawing.Size(512, 20);
+            this.EventTopicTextBox.TabIndex = 2;
+            // 
+            // EventTopicLabel
+            // 
+            this.EventTopicLabel.AutoSize = true;
+            this.EventTopicLabel.Location = new System.Drawing.Point(38, 60);
+            this.EventTopicLabel.Name = "EventTopicLabel";
+            this.EventTopicLabel.Size = new System.Drawing.Size(65, 13);
+            this.EventTopicLabel.TabIndex = 3;
+            this.EventTopicLabel.Text = "Event Topic";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 352);
+            this.Controls.Add(this.EventTopicLabel);
+            this.Controls.Add(this.EventTopicTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sendEventButton);
+            this.Name = "Form1";
+            this.Text = "Event Grid Tester";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sendEventButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox EventTopicTextBox;
+        private System.Windows.Forms.Label EventTopicLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventGridWindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            EventTopicTextBox.Text = "https://xxxxxxxxxxxxxxxxxx.eastus-1.eventgrid.azure.net/api/events";
+        }
+
+        private async void sendEvent_Click(object sender, EventArgs e)
+        {
+            var responseMessage = await MakeRequestEvent();
+            MessageBox.Show($"Event sent. Status Code: {responseMessage.StatusCode}");
+        }
+
+        private async Task<HttpResponseMessage> MakeRequestEvent()
+        {
+            string topicEndpoint = EventTopicTextBox.Text;
+
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("aeg-sas-key", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+            List<CustomEvent<SampleEventData>> events = new List<CustomEvent<SampleEventData>>();
+            var customEvent = new CustomEvent<SampleEventData>();
+            customEvent.EventType = "eventgridtest";
+            customEvent.Subject = "Event Grid Test";
+            customEvent.Data = new SampleEventData() { Name = "Parker" };
+            events.Add(customEvent);
+            var jsonContent = JsonConvert.SerializeObject(events);
+            Console.WriteLine(jsonContent);
+
+            var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+            return await httpClient.PostAsync(topicEndpoint, content);
+        }
+
+    }
+    public class CustomEvent<T>
+    {
+
+        public string Id { get; private set; }
+
+        public string EventType { get; set; }
+
+        public string Subject { get; set; }
+
+        public string EventTime { get; private set; }
+
+        public T Data { get; set; }
+
+        public CustomEvent()
+        {
+            Id = Guid.NewGuid().ToString();
+
+            DateTime localTime = DateTime.Now;
+            DateTime utcTime = DateTime.UtcNow;
+            DateTimeOffset localTimeAndOffset = new DateTimeOffset(localTime, TimeZoneInfo.Local.GetUtcOffset(localTime));
+            EventTime = localTimeAndOffset.ToString("o");
+        }
+    }
+
+    public class SampleEventData
+    {
+        public string Name { get; set; }
+    }
+}
